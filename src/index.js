1c4829f1abceb31914457a62a074103a00857183
Manuel Reich <mr@wunderundfitzig.de>
2017-11-15 22:38:40 +0100

    Add orientation and a some lights

--- src/index.js
+++ src/index.js
@@ -11,22 +11,38 @@ const renderer = new three.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const geometry = new three.BoxGeometry(0.5, 2, 1)
-const material = new three.MeshBasicMaterial({ color: 'grey' })
+var ambientLight = new three.AmbientLight(0x0c0c0c)
+scene.add(ambientLight)
+
+var spotLight = new three.SpotLight(0xffffff)
+spotLight.position.set(-30, 60, 60)
+spotLight.castShadow = true
+scene.add(spotLight)
+
+const geometry = new three.BoxGeometry(1, 2, 0.2)
+const material = new three.MeshLambertMaterial({ color: 'white' })
 const cube = new three.Mesh(geometry, material)
 scene.add(cube)
 
-camera.position.z = 20
-cube.rotation.x = 2
-cube.rotation.y = 2
+camera.position.z = 5
 
-socket.on('position', msg => {
-  const arr = JSON.parse(msg)
-  const x = arr[0]
-  const y = arr[1]
-  const z = arr[2]
-  console.log(x, y, z)
-  cube.position.set(x, y, z)
+cube.rotation.x = -0.1
+cube.rotation.y = 0.5
+
+// socket.on('position', msg => {
+//   const [x, y, z] = JSON.parse(msg)
+//   console.log('position', x, y, z)
+//
+//   cube.position.set(x, y, z)
+//   renderer.render(scene, camera)
+// })
+
+socket.on('rotation', msg => {
+  const [x, y, z, w] = JSON.parse(msg)
+
+  const quaternion = new three.Quaternion(x, y, z, w)
+  cube.setRotationFromQuaternion(quaternion)
+
   renderer.render(scene, camera)
 })
 
